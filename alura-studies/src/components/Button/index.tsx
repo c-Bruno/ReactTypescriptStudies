08a343ca3d6ae781b83254680a167e4d72a3999b
bcaboclo <bcaboclo@cpqd.com.br>
2022-11-28 19:42:44 -0300

    Refatorado botao e formulario, tornando eles function components

--- alura-studies/src/components/Button/index.tsx
+++ alura-studies/src/components/Button/index.tsx
@@ -1,35 +1,48 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component <{ 
-    children: any, 
+interface Props {
     type?: "button" | "submit" | "reset" | undefined
     onClick?: () => void
-}>{
-    render() {
-        const {type = 'button', onClick} = this.props;
-        // const backgroundColor = 'green';
-
-        // const styles = {
-        //     backgroundColor
-        // }
-        
-        return (
-            <button onClick={onClick} type={type} className={style.botao}
-            // className='botao'
-            // style=
-            // // Formas de estilar CSS mas o INLINE STYLE não é uma boa pratica
-            // // {{
-            // //     // backgroundColor: 'blue'
-            // //     // backgroundColor: backgroundColor
-            // //     backgroundColor
-            // // }}
-            // {styles}
-            >
-                {this.props.children}
-            </button>
-        )
-    }
+    children?: React.ReactNode
 }
 
-export default Botao;
+export default function Botao({onClick, type, children}: Props){
+    return (
+        <button onClick={onClick} type={type} className={style.botao}        >
+            {children}
+        </button>
+    )
+}
+// class Botao1 extends React.Component <{ 
+//     children: any, 
+//     type?: "button" | "submit" | "reset" | undefined
+//     onClick?: () => void
+// }>{
+//     render() {
+//         const {type = 'button', onClick} = this.props;
+//         // const backgroundColor = 'green';
+
+//         // const styles = {
+//         //     backgroundColor
+//         // }
+        
+//         return (
+//             <button onClick={onClick} type={type} className={style.botao}
+//             // className='botao'
+//             // style=
+//             // // Formas de estilar CSS mas o INLINE STYLE não é uma boa pratica
+//             // // {{
+//             // //     // backgroundColor: 'blue'
+//             // //     // backgroundColor: backgroundColor
+//             // //     backgroundColor
+//             // // }}
+//             // {styles}
+//             >
+//                 {this.props.children}
+//             </button>
+//         )
+//     }
+// }
+
+// export default Botao;
--- alura-studies/src/components/Formulario/index.tsx
+++ alura-studies/src/components/Formulario/index.tsx
@@ -1,41 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Itarefa } from "../../types/tarefa";
 import Botao from "../Button";
 import style from './Formulario.module.scss';
 import { v4 as uuidv4 } from 'uuid';
 
-class Formulario extends React.Component<{
+interface Props {
     setTarefas: React.Dispatch<React.SetStateAction<Itarefa[]>>
-}> {
-    state = {
-        tarefa: '',
-        tempo: '00:00:00'
-    }
-
-    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
-        evento.preventDefault();
-        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state, selecionado: false, completado: false, id: uuidv4()}])
-        this.setState({
-            tarefa: '',
-            tempo: '00:00:00'
-        })
-    }
-
-    render(): React.ReactNode {
-        return (
-            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
-                <div className={style.inputContainer}>
-                    <label htmlFor="tarefa">Adicione um novo estudo</label>
-                    <input type="text" value={this.state.tarefa} name="tarefa" id="tarefa" placeholder="O que você quer estudar" required onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}/>
-                </div>
-                <div className={style.inputContainer}>
-                    <label htmlFor="tempo">Tempo</label>
-                    <input type="time" value={this.state.tempo} step={1} name="tempo" id="tempo" min={'00:00:00'} max={'01:30:00'} required onChange={evento => this.setState({...this.state, tempo: evento.target.value})}/>
-                </div>
-                <Botao type="submit">Adicionar</Botao>
-            </form>
-        )
-    }
 }
 
-export default Formulario;
+export default function Formulario({ setTarefas}: Props) {
+    const [tarefa, setTarefa] = useState("");
+    const [tempo, setTempo] =  useState("00:00");
+
+    function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        setTarefas(tarefasAntigas => [...tarefasAntigas, {tarefa, tempo, selecionado: false, completado: false, id: uuidv4()}])
+
+        setTarefa("");
+        setTempo("00:00");
+    }
+
+    return (
+            <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
+            <div className={style.inputContainer}>
+                <label htmlFor="tarefa">Adicione um novo estudo</label>
+                <input type="text" value={tarefa} name="tarefa" id="tarefa" placeholder="O que você quer estudar" required onChange={evento => setTarefa(evento.target.value)}/>
+            </div>
+            <div className={style.inputContainer}>
+                <label htmlFor="tempo">Tempo</label>
+                <input type="time" value={tempo} step={1} name="tempo" id="tempo" min={'00:00:00'} max={'01:30:00'} required onChange={evento => setTempo(evento.target.value)}/>
+            </div>
+            <Botao type="submit">Adicionar</Botao>
+        </form>
+    )
+}
